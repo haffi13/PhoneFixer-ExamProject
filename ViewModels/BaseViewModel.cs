15bc73ddecf73be0d26d5e7b47a2de126fc48fd2
Hafsteinn Fannar <haffi13@gmail.com>
2018-04-17 11:25:05 +0200

    Added a BaseViewModel to implement INotifyPropertyChanged for the ViewModels.

---
+++ ViewModels/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ViewModels
+{
+    // BaseViewModel is inherited by all other view models.
+    // It saves on implementing INotifyPropertyChanged individually for each view model.
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // ([CallerMemberName]) gives OnPropertyChanged the name of the calling member
+        // without it having to be passed as a parameter to the method.
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
