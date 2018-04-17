d40dd2adf724a4108563966d0a02d930cd66fd38
Hafsteinn Fannar <haffi13@gmail.com>
2018-04-17 11:30:25 +0200

    Added a RealyCommand class to the ViewModels. Implements the ICommand interface to be used by the view models.

---
+++ ViewModels/RelayCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ViewModels
+{
+    // This class is used to relay commands from the Views to the ViewModels.
+    // Is used instead of inheriting the ICommand interface for each view model.
+    public class RelayCommand : ICommand
+    {
+        private Action _action;
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        // REVIEW!! !! ! !!  !
+        public event EventHandler CanExecuteChanged = (sender, e) => { };   //--Seems to work without the sender..is never used in current solution
+        //only the CanExecute and Execute are used.
+
+        // Could this method be used to make StartSelection available in MainWindow.
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
