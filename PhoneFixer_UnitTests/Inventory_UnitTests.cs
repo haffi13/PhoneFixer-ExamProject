4308fafbbe386e1dbe018f7a99aab461a8ac3b58
Hafsteinn Fannar <haffi13@gmail.com>
2018-04-23 16:35:36 +0200

    Started writing unit tests. Changed the name of the ObservableCollection the tab control is bound to from TabItems to TabViewModels to avoid confusion with inventory Items.

--- PhoneFixer_UnitTests/Inventory_UnitTests.cs
+++ PhoneFixer_UnitTests/Inventory_UnitTests.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewModels;
+using Models;
+using System.Collections.ObjectModel;
 
 namespace PhoneFixer_UnitTests
 {
     [TestClass]
     public class Inventory_UnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        MainWindowViewModel mainWindowViewModel;
+        InventoryViewModel inventoryViewModel;
+        ObservableCollection<ITabItem> tabItem = new ObservableCollection<ITabItem>();
+
+
+        [TestInitialize]
+        public void InitializeTest()
         {
+            tabItem.Add(new InventoryViewModel { Header = "Inventory" });
+            tabItem.Add(new InventoryViewModel { Header = "Service" });
+            mainWindowViewModel = new MainWindowViewModel();
+            inventoryViewModel = new InventoryViewModel();
+        }
+
+        [TestMethod]
+        public void TabItemHeaderRegistersCorrectly()
+        {
+            Assert.AreEqual(tabItem[0].Header, mainWindowViewModel.TabViewModels[0].Header);
+            Assert.AreEqual(tabItem[1].Header, mainWindowViewModel.TabViewModels[1].Header);
+        }
+
+        [TestMethod]
+        public void Tas()
+        {
+            
         }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -11,26 +11,24 @@ namespace ViewModels
     {
         // Gets populated with tab items. The UI gets the Header and the Content
         // of the tabs from here. Header is the title, Content is the ViewModel.
-        private ObservableCollection<ITabItem> tabItems = new ObservableCollection<ITabItem>();
+        private ObservableCollection<ITabItem> tabViewModels = new ObservableCollection<ITabItem>();
 
         // Maybe change the name of TabItems to be more specific/descriptive.
-        public ObservableCollection<ITabItem> TabItems
+        public ObservableCollection<ITabItem> TabViewModels
         {
-            get { return tabItems; }
+            get { return tabViewModels; }
             set
             {
-                tabItems = value;
+                tabViewModels = value;
                 OnPropertyChanged();
             }
         }
 
         public MainWindowViewModel()
         {
-            TabItems.Add(new InventoryViewModel { Header = "Inventory" });
-            TabItems.Add(new ServiceViewModel   { Header = "Service" });
-            TabItems.Add(new CustomerViewModel  { Header = "Customers" });
-            
-        }
-        
+            TabViewModels.Add(new InventoryViewModel { Header = "Inventory" });
+            TabViewModels.Add(new ServiceViewModel   { Header = "Service" });
+            TabViewModels.Add(new CustomerViewModel  { Header = "Customers" });   
+        }   
     }
 }
