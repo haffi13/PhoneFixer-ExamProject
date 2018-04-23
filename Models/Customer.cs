4877d7b84deb0d520c12f2cda72a786df77b49d2
tux-S1lv3r <faiz0095@edu.eal.dk>
2018-04-23 11:38:53 +0200

    Added Customer and Service class.

---
+++ Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Customer
+    {
+        //int
+        public int CustomerID { get; set; }
+
+        //Nvarchar(50)?
+        public string Name { get; set; }
+
+        //Nvarchar(50)
+        public string Tlf { get; set; }
+
+        //Nvarchar(50)
+        public string Email { get; set; }
+
+        public bool Subscribed = false;
+
+        int AmountOfSales = 0;
+        int AmountSpent = 0;
+    }
+}
---
+++ Models/Service.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    class Service
+    {
+        //int
+        public int ServiceID { get; set; }
+
+        //Nvarchar(150)
+        public string Description { get; set; }
+
+        //decimal(18, 2)
+        public decimal Price { get; set; }
+
+        public bool Done = false;
+        public bool Pickup = false;
+    }
+}
