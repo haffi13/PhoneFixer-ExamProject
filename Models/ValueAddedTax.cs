8e313198856c912e40752a90837d12c9d0ed8581
tux-S1lv3r <faiz0095@edu.eal.dk>
2018-05-02 15:04:51 +0200

    VAT

---
+++ Models/ValueAddedTax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public static class ValueAddedTax
+    {
+        public static decimal AddVAT(decimal input)
+        {
+            return input * 1.25m;
+        }
+
+        public static decimal RemoveVAT(decimal input)
+        {
+            return input * 0.80M;
+        }
+    }
+}
