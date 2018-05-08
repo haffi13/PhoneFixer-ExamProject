99dd64634ee735ae736a67b3403324ece6d2e3c4
tux-S1lv3r <faiz0095@edu.eal.dk>
2018-05-08 13:22:07 +0200

    edit

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -9,7 +9,6 @@ namespace ViewModels
         // of the tabs from here. Header is the title, Content is the ViewModel.
         private ObservableCollection<ITabItem> tabViewModels = new ObservableCollection<ITabItem>();
         private ITabItem selectedTab;
-        
         // Collection of view models inheriting the ITabItem inteface. 
         // Those view models are then displayed in corresponding tabs in the MainWindow.
         // The tab control in the MainWindow is bound to this property.
